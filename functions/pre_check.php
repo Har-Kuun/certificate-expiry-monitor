40ba5b96f8fbc442435f7539c36dfcec189da5b7
Har-Kuun <Har-Kuun@users.noreply.github.com>
2018-08-30 14:44:47 -0500

    Translate pre_check.php

--- functions/pre_check.php
+++ functions/pre_check.php
@@ -24,36 +24,36 @@ function add_domain_to_pre_check($domain,$email,$visitor_ip) {
     $email = trim($email);
     $file = file_get_contents($pre_check_file);
     if ($file === FALSE) {
-        $result['errors'][] = "Can't open database.";
+        $result['errors'][] = "无法打开数据库。";
         return $result;
     }
     $json_a = json_decode($file, true);
     if ($json_a === null && json_last_error() !== JSON_ERROR_NONE) {
-        $result['errors'][] = "Can't read database: " . htmlspecialchars(json_last_error());
+        $result['errors'][] = "无法读取数据库: " . htmlspecialchars(json_last_error());
         return $result;
     }
 
     foreach ($json_a as $key => $value) {
         if ($value["domain"] == $domain && $value["email"] == $email) {
-            $result['errors'][] = "Domain/email combo for  " . htmlspecialchars($domain) . " already exists. Please confirm your subscription email.";
+            $result['errors'][] = "域名/邮件组合  " . htmlspecialchars($domain) . " 已经存在。请在您的邮箱中点击确认链接。";
             return $result;
         }
     }
 
     $check_json_file = file_get_contents($check_file);
     if ($check_json_file === FALSE) {
-        $result['errors'][] = "Can't open database.";
+        $result['errors'][] = "无法打开数据库。";
         return $result;
     }
     $check_json_a = json_decode($check_json_file, true);
     if ($check_json_a === null && json_last_error() !== JSON_ERROR_NONE) {
-        $result['errors'][] = "Can't read database: " . htmlspecialchars(json_last_error());
+        $result['errors'][] = "无法读取数据库: " . htmlspecialchars(json_last_error());
         return $result;
     }
 
     foreach ($check_json_a as $key => $value) {
         if ($value["domain"] == $domain && $value["email"] == $email) {
-            $result['errors'][] = "Domain / email combo for  " . htmlspecialchars($domain) . " already exists.";
+            $result['errors'][] = "域名/邮件组合  " . htmlspecialchars($domain) . " 已经存在。";
             return $result;
         }
     }
@@ -69,15 +69,15 @@ function add_domain_to_pre_check($domain,$email,$visitor_ip) {
     if(file_put_contents($pre_check_file, $json, LOCK_EX)) {
         $result['success'][] = true;
     } else {
-        $result['errors'][] = "Can't write database.";
+        $result['errors'][] = "无法写入数据库。";
         return $result;
     }
 
     $sublink = "https://" . $current_link . "/confirm.php?id=" . $uuid;
 
     $to      = $email;
-    $subject = "Confirm your Certificate Expiry Monitor subscription for " . htmlspecialchars($domain) . ".";
-    $message = "Hello,\r\n\r\nSomeone, hopefully you, has added his website to the Certificate Expiry Monitor. This is a service which monitors an SSL certificate on a website, and notifies you when it is about to expire. This extra notification helps you remember to renew your certificate on time.\r\n\r\nIf you have subscribed to this check, please click the link below to confirm this subscription. If you haven't subscribed to the Certificate Expiry Monitor service, please consider this message as not sent.\r\n\r\n\r\nDomain: " . trim(htmlspecialchars($domain)) . "\r\nEmail: " . trim(htmlspecialchars($email)) . "\r\nIP subscribed from: " . htmlspecialchars($visitor_ip) . "\r\nDate subscribed: " . date("Y-m-d H:i:s T") . "\r\n\r\nPlease click or copy and paste the below link in your browser to subscribe: \r\n\r\n" . $sublink . "\r\n\r\n\r\nHave a nice day,\r\nThe Certificate Expiry Monitor Service.";
+    $subject = "请确认域名 " . htmlspecialchars($domain) . " 的网站证书过期提醒";
+    $message = "您好，\r\n\r\n您申请使用网站证书过期检测提醒服务。\r\n\r\n请点击链接确认该服务。如果您没有申请过我们的服务，请无视这封邮件。\r\n\r\n\r\n域名: " . trim(htmlspecialchars($domain)) . "\r\n邮箱: " . trim(htmlspecialchars($email)) . "\r\nIP地址: " . htmlspecialchars($visitor_ip) . "\r\n日期: " . date("Y-m-d H:i:s T") . "\r\n\r\n请点击下面的链接确认使用我们的服务: \r\n\r\n" . $sublink . "\r\n\r\n\r\n祝您健康愉快,\r\n网站证书过期检测提醒 by 香菇肥牛";
     $message = wordwrap($message, 70, "\r\n");
     $headers = 'From: noreply@' . $current_domain . "\r\n" .
         'Reply-To: noreply@' . $current_domain . "\r\n" .
@@ -92,7 +92,7 @@ function add_domain_to_pre_check($domain,$email,$visitor_ip) {
     if (mail($to, $subject, $message, $headers) === true) {
         $result['success'][] = true;
     } else {
-        $result['errors'][] = "Can't send email.";
+        $result['errors'][] = "无法发送邮件。";
         return $result;
     }
 
