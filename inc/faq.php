a1675c137733f6c7031d9343706734c95930e8f9
Har-Kuun <Har-Kuun@users.noreply.github.com>
2018-08-30 11:12:01 -0500

    Translate faq.php

--- inc/faq.php
+++ inc/faq.php
@@ -18,52 +18,52 @@
 
 <hr>
 
-<h1>FAQ</h1>
+<h1>常见问题</h1>
 
-<h2>Is this service free?</h2>
-<p>Yes, this service is free. You can add as many domains as you like, however, don't make it excessive. If you do, we might contact you to make an arrangement.</p><br>
+<h2>该服务是否免费？</h2>
+<p>该服务永久免费，不过如果您添加了特别多的域名，我们可能会联系您作额外的安排。</p><br>
 
-<h2>How often do you check a cert?</h2>
-<p>The check will run at least once every 2 days, but most of the time daily.</p><br>
+<h2>证书过期检测的频率如何？</h2>
+<p>我们至少每两天检测一次；通常情况下我们每天检测一次。</p><br>
 
-<h2>When will you email me?</h2>
-<p>We will email you on the following events:<br>
+<h2>我何时会收到邮件通知？</h2>
+<p>我们将在以下的时间节点通知您：<br>
 <ul>
-  <li>When you sign up, to confirm the domain(s).</li>
-  <li>If a certificate expires in:</li>
+  <li>当您注册的时候以便确认您的域名；</li>
+  <li>当您的证书将在以下的时间段后过期：</li>
   <ul>
-    <li>90 days (3 months)</li>
-    <li>60 days (2 months)</li>
-    <li>30 days (1 month)</li>
-    <li>14 days (2 weeks)</li>
-    <li>7 days (1 week)</li>
-    <li>5 days</li>
-    <li>3 days</li>
-    <li>2 days</li>
-    <li>1 day</li>
+    <li>90 天 (3个月)</li>
+    <li>60 天 (2个月)</li>
+    <li>30 天 (1个月)</li>
+    <li>14 天 (2 周)</li>
+    <li>7 天 (1 周)</li>
+    <li>5 天</li>
+    <li>3 天</li>
+    <li>2 天</li>
+    <li>1 天</li>
   </ul>
-  <li>The day your certificate expires.</li>
-  <li>2 days after your certificated expired, and has not been replaced yet.</li>
-  <li>7 days after your certificated expired, and has not been replaced yet.</li>
-  <li>If we cannot connect to your site.</li>
-  <li>If we cannot connect to your site for 7 days in a row, we'll delete the check.</li>
+  <li>您证书过期的当天；</li>
+  <li>您的证书过期两天以后且仍未更新证书；</li>
+  <li>您的证书过期七天以后且仍未更新证书；</li>
+  <li>您的站点无法访问；</li>
+  <li>您的站点连续七天无法访问，此后我们将不再检测您的网站的证书。</li>
 </ul>
-  If you replace your certificate before it expires, we'll stop emailing you until the new certificate expires again.<br>
-  We will never spam you or sell your data to a third party.<br>
+  如果您在到期前更换/续期了您的证书，我们将停止给您发送邮件，直到您新的证书过期。<br>
+  我们绝对不会给您发送垃圾邮件或者将您的信息售卖给第三方。<br>
   </p><br>
 
 
-<h2>Do you check all certificates in the chain?</h2>
-<p>Yes. All certificates in the chain are checked, a maximum of 10. You will receive notification if any of the chain certificates expire as well.</p><br>
+<h2>该服务是否检测证书链中的全部证书？</h2>
+<p>是的，我们会检测证书链中的全部证书，最多检测10级证书链。如果链中的某个证书即将过期，您也会收到关于该证书的过期提醒。</p><br>
 
-<h2>Do you provide any guarantees on uptime?</h2>
-<p>We provide this service on a best effort base. The project is fully open source, you can set up your own instance if you demand 100% uptime.</p><br>
+<h2>你们是否有在线率保证？</h2>
+<p>我们尽最大努力提供最好的服务。本项目完全开源，如果您需要100%的在线率保证，可以自行搭建该程序。</p><br>
 
-<h2>What license is the project under?</h2>
-<p>GNU Affero GPL v3 or later.</p><br>
+<h2>该项目基于何证书？</h2>
+<p>GNU Affero GPL v3 或更新版本.</p><br>
 
-<h2>Do you have any tips for secure certificate configuration?</h2>
-<p>Yes. You can check out <a href="https://cipherli.st">Cipherli.st</a> for secure server settings and guides. You can also use the <a href="https://ssldecoder.org">SSL Decoder</a> to check your current setup.</p><br>
+<h2>我想获得更多关于安全部署证书的方法？</h2>
+<p>您可以查看这个链接 <a href="https://cipherli.st">Cipherli.st</a>, 它提供了服务器安全设置的教程。您还可以查看这个网站 <a href="https://ssldecoder.org">SSL Decoder</a> 检测您当前的证书设置情况。</p><br>
 
 <h2></h2>
 <p></p><br>
